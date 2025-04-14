4bde56749a522ce4b5c81c35740baf2cf7aa5ba6
kaveesha <kaveeshasewmini161@gmail.com>
2025-04-14 10:36:02 +0530

    SQL connection setup

--- code/signin.php
+++ code/signin.php
@@ -1,9 +1,22 @@
+<?php
+    include ("connection.php");
+
+    $msg='';
+
+    if(isset($_POST['submit'])){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $select1 = "SELECT * FROM 'users' WHERE email= '$email' AND password='$password';";
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Signin</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 
     <!-- Bootstrap CSS -->
@@ -38,7 +51,7 @@
                         <input type="password" class="form-control" id="password">
                     </div>
                     <div class="text-center">
-                      <button type="submit" class="btn btn-primary" style="margin:10px; width:40%;">Login</button>
+                      <button type="submit" name="submit" class="btn btn-primary" style="margin:10px; width:40%;">Login</button>
                     </div>
                     <div class="text-center">
                       <p style="margin-bottom:30px;">Don't have an account? <a href="register.php">Create an account</a></p>
